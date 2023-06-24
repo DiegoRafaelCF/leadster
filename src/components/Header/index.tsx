230f2efd8a55feaa5fe967602b3de3c9120b2ef1
Diego Rafael <drafael8000@gmail.com>
2023-06-24 19:24:45 -0300

    feat: apply the second part of the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,34 @@
-import { HeaderContainer, Logo } from './style';
+import {
+  HeaderContainer,
+  InformationTag,
+  Logo,
+  MainHeader,
+  MoreConversionContainer,
+  SecondaryHeader,
+  SubTitle,
+  Title,
+} from './style';
 
 export function Header(): JSX.Element {
   return (
     <HeaderContainer>
-      <Logo src="/images/logo.png" alt="leadster-logo" />
+      <MainHeader>
+        <Logo src="/images/logo.png" alt="leadster-logo" />
+      </MainHeader>
+      <SecondaryHeader>
+        <InformationTag>WEBNARS EXCLUSIVOS</InformationTag>
+        <MoreConversionContainer>
+          <Title>
+            Menos Conversinha,
+            <br />
+            <strong>Mais conversão</strong>
+          </Title>
+        </MoreConversionContainer>
+        <SubTitle>
+          Conheça as estratégias que <strong>mudaram o jogo</strong> e como
+          aplicá-las no seu negócio
+        </SubTitle>
+      </SecondaryHeader>
     </HeaderContainer>
   );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,9 +1,21 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div`
+/* 
+  To the header component I preferred to use 3 breakpoints.
+  They are:
+  -> max-width: 768px;
+  -> min-width: 768px and max-width: 1024px;
+  -> min-width: 1024px;
+*/
+
+export const HeaderContainer = styled.section`
+  width: 100%;
+`;
+
+export const MainHeader = styled.div`
   width: 100%;
   height: 80px;
-  background-color: #ffffff;
+  background-color: var(--primary-bg-color);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -11,4 +23,124 @@ export const HeaderContainer = styled.div`
 
 export const Logo = styled.img`
   width: 170px;
+
+  @media (max-width: 768px) {
+    width: 132px;
+  }
+`;
+
+export const SecondaryHeader = styled.div`
+  width: 100%;
+  padding: 150px 0px 150px 0px;
+  background-color: var(--secondary-bg-color);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 1024px) {
+    padding: 60px 20px 60px 20px;
+  }
+`;
+
+export const InformationTag = styled.span`
+  padding-left: 12px;
+  padding-right: 12px;
+  margin-bottom: 10px;
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  font-weight: bold;
+  font-size: 12px;
+  color: var(--secondary-blue);
+  border: 2px solid var(--secondary-blue);
+  border-radius: 45px 45px 45px 0px;
+
+  @media (max-width: 768px) {
+    font-size: 10px;
+  }
+`;
+
+export const MoreConversionContainer = styled.div`
+  position: relative;
+
+  ::after {
+    background-image: url('/images/asset-header.png');
+    background-size: cover;
+    width: 40px;
+    height: 26px;
+    position: absolute;
+    right: -12px;
+    bottom: 52px;
+    content: '';
+  }
+
+  @media (max-width: 768px) {
+    padding: 0px 10px 10px 10px;
+
+    ::after {
+      width: 24px;
+      height: 15px;
+      right: 0px;
+      bottom: 38px;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    ::after {
+      width: 32px;
+      height: 20px;
+      bottom: 40px;
+    }
+  }
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  line-height: 1.2;
+  font-weight: 500px;
+  font-size: 40px;
+  color: var(--primary-gray);
+
+  strong {
+    line-height: 1.2;
+    font-weight: 700px;
+    font-size: 76px;
+    color: var(--secondary-blue);
+  }
+
+  @media (max-width: 768px) {
+    font-size: 22px;
+
+    strong {
+      font-size: 40px;
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    font-size: 36px;
+
+    strong {
+      font-size: 58px;
+    }
+  }
+`;
+
+export const SubTitle = styled.h2`
+  margin-top: 10px;
+  padding: 10px 20px 0px 20px;
+  border-top: 1px solid var(--secondary-gray);
+  text-align: center;
+  font-family: 'Plus Jakarta Sans', sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  color: var(--primary-gray);
+
+  @media (max-width: 768px) {
+    max-width: 360px;
+    font-size: 12px;
+  }
+
+  @media (min-width: 768px) and (max-width: 1024px) {
+    max-width: 500px;
+  }
 `;
