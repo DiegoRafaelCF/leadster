af4a3a1e1b6b897156f862f20fa2686b9d26582f
Diego Rafael <drafael8000@gmail.com>
2023-06-29 16:43:32 -0300

    feat: create and apply the message to use it on empty pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="w-full flex items-center justify-center">
-      <span>Home</span>
+      <div className="font-PJ my-32">
+        <h1 className="text-blue-secondary-blue font-bold text-5xl">
+          Desculpe pela bagunça!
+        </h1>
+        <h2 className="text-gray-primary-gray font-semibold text-lg mt-4 mb-8 ">
+          Estamos construindo uma página incrível para você :)
+        </h2>
+        <span className="flex gap-1 text-gray-primary-gray font-medium text-md">
+          Em breve! Fique ligado para mais informações em
+          <Link
+            className="text-blue-primary-blue"
+            rel="stylesheet"
+            href="https://leadster.com.br/"
+            target="_blank"
+          >
+            leadser.com.br
+          </Link>
+        </span>
+      </div>
     </main>
   );
 }
