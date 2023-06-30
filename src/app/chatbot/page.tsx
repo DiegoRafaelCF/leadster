f318440f401973443c8045c581730674576b3769
Diego Rafael <drafael8000@gmail.com>
2023-06-30 00:05:18 -0300

    fix: 'sorry message' from pages

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -1,27 +1,17 @@
 'use client';
 
-import Link from 'next/link';
-
 export default function Chatbot() {
   return (
-    <main className="w-full flex items-center justify-center p-16">
-      <div className="font-PJ my-32">
-        <h1 className="text-blue-secondary-blue font-bold text-5xl">
+    <main className="w-full flex items-center justify-center">
+      <div className="font-PJ my-32 max-[600px]:my-16 px-4">
+        <h1 className="text-blue-secondary-blue font-bold text-5xl max-[600px]:text-2xl">
           Desculpe pela bagunça!
         </h1>
-        <h2 className="text-gray-primary-gray font-semibold text-lg mt-4 mb-8 ">
+        <h2 className="text-gray-primary-gray font-semibold text-lg max-[600px]:text-sm mt-4 mb-8 ">
           Estamos construindo uma página incrível para você :)
         </h2>
-        <span className="flex gap-1 text-gray-primary-gray font-medium text-md">
-          Em breve! Fique ligado para mais informações em
-          <Link
-            className="text-blue-primary-blue"
-            rel="stylesheet"
-            href="https://leadster.com.br/"
-            target="_blank"
-          >
-            leadser.com.br
-          </Link>
+        <span className="flex gap-1 text-gray-primary-gray font-medium text-md max-[600px]:text-sm">
+          Em breve! Fique ligado para mais informações.
         </span>
       </div>
     </main>
